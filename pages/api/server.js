b95692297d32507cbe4472eb43f755f7a6fbbb42
qkwkqhtn <qkwkqhtn@naver.com>
2022-02-24 11:50:48 +0900

    Update server.js

--- pages/api/server.js
+++ pages/api/server.js
@@ -11,31 +11,8 @@ mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log("MongoDB connected..."))
 .catch((e) => console.log("MongoDB error...", e))
 
-let db = mongoose.connection;
-let testData = {name: 'sdasd', age: '22', address: 'sdjidjsjidsji'};
-db.collection("datas").insertOne(testData,(err, res) => {
-    if(err) throw err;
-    console.log("1 document inserted..");
-})
+const db = mongoose.connection;
 
-/*
-let student = mongoose.Schema({
-    name: 'string',
-    address : 'string',
-    age : 'number'
-});
-
-let alertData = mongoose.model('data', student);
-let newalertData = new alertData();
-
-newStudent.save((err, data) => {
-    if(err){
-        console.log("error");
-    }else{
-        console.log('save')
-    }
-});
-*/
 const io = socketIO(server,{
     cors: {
         origin: "*",
@@ -50,6 +27,10 @@ io.on('connection', (socket) => {
     socket.on("webhook",msg =>{
         io.emit("webhooktest", msg);
         io.emit("tradeData", msg);
+        db.collection("datas").insertOne(msg,(err, res) => {
+            if(err) throw err;
+            console.log("1 document inserted..");
+        })
         console.log("성공이다 ", msg);
     });
 });
