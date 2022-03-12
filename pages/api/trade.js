aca98dc99d2ea7727cc1ca48f74e3544a94c16cd
JangSeongHun99 <qkwkqhtn@naver.com>
2022-03-12 17:26:59 +0900

    20220312
    20220312

--- pages/api/trade.js
+++ pages/api/trade.js
@@ -25,20 +25,27 @@ async function coinAmount(){
     return(data1.toFixed(3)-0.001);
 }
 
-async function futuresMarketBuy(){
+async function futuresPositionLong(){
     const amount = await coinAmount()
     binance.futuresMarketBuy('ETHUSDT',amount);// 갯수
-   
-   //console.log(data);
-
     console.log("구매완료");
 }
-async function futuresMarketSell(){
+async function futuresPositionShot(){
     const amount = await coinAmount()
     binance.futuresMarketSell('ETHUSDT',amount);// 갯수
 }
+async function futuresMarketSell(){
+    const account = await futuresAccount();
+    const amount= account.positionAmt;
+    binance.futuresMarketSell('ETHUSDT',amount);
+}
+async function futuresMarketBuy(){
+    const account = await futuresAccount();
+    const amount= account.positionAmt;
+    binance.futuresMarketLong('ETHUSDT',amount);
+}
 
-futuresAccount();
+//futuresAccount();
 //futuresMarketBuy(account);
 
 
@@ -46,26 +53,27 @@ futuresAccount();
 socket.on("tradeData", async data =>{
     const account = await futuresAccount()
     console.log(account);
-    /*
+    
     if(account.positionAmt > 0){
         if(data.divergenceSellCircle && data.rsi >= -20){
             await futuresMarketSell();
-            await futuresMarketSell();
+            await futuresPositionShot();
+            return
         }
         if(data.divergenceBuyCircle && data.rsi <= -80){
             await futuresMarketBuy();
-            await futuresMarketBuy();
+            await futuresPositionLong();
+            return
         }
     }
     if(account.positionAmt == 0){
         if(data.divergenceSellCircle && data.rsi >= -20){
-            futuresMarketSell();
+            futuresPositionShot();
+            return
         }
         if(data.divergenceBuyCircle && data.rsi <= -80){
-            futuresMarketBuy();
+            futuresPositionLong();
+            return
         }
     }
-    */
-
 })
-
